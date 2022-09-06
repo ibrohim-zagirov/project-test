5423ae1eab143ca53f56ec352269d4062466ce32
Ibrohim <72086540+ibrohim-zagirov@users.noreply.github.com>
2022-09-06 08:34:09 +0300

    commit

--- src/app/sevices/list.service.ts
+++ src/app/sevices/list.service.ts
@@ -1,11 +1,19 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
+import { map, Observable } from "rxjs";
+import { SortingIntrface } from "../types/sorting.intrface";
 
 @Injectable()
 export class ListService {
     constructor(private http: HttpClient) { }
-    getList(): Observable<any> {
-        return this.http.get('https://jsonplaceholder.typicode.com/todos')
+    getList(user: SortingIntrface): Observable<any> {
+        return this.http.get('https://jsonplaceholder.typicode.com/users')
+    }
+
+    getById(id: string): Observable<any> {
+        return this.http.get(`${'https://jsonplaceholder.typicode.com/users'}/card/${id}.json`)
+            .pipe(map((: SortingIntrface) => {
+                return
+            }))
     }
 }
